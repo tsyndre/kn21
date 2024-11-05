dcfa0ee9e426b868e20a9bb87d26be1eea893ab2
tsyndre <tsynderebou@gmail.com>
2024-11-05 14:16:54 +0200

    dodav cod

---
+++ 2/gith.py
@@ -0,0 +1,9 @@
+while True :
+    msg=input("please input a msg")
+    if msg=="добрий день":
+      print("добрий день")
+    elif msg=="слава україна":
+      print("героям слава")
+    elif msg=="pa pa ":
+      print("the eng")
+      break
